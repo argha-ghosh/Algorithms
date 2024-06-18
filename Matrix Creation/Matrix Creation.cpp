bef30ec798d9e38d0c8f1a326602114d0861f328
ARGHA GHOSH <147973839+argha-ghosh@users.noreply.github.com>
2024-06-18 21:18:50 +0600

    Add files via upload

---
+++ Matrix Creation/Matrix Creation.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printmatrix(int **p, int r,int c)
+{
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            cout<<p[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int row;
+    int column;
+    cout<<"Enter the row: " ;
+    cin>>row;
+    cout<<"Enter the column: " ;
+    cin>>column;
+    srand (time(0));
+    int **m= new int*[row];
+    for(int i=0; i<row; i++)
+    {
+        m[i]=new int [column];
+    }
+
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            m[i][j]=rand();
+        }
+    }
+    cout<<endl;
+    cout<<"Here is the matrix: "<<endl;
+    printmatrix(m,row,column);
+    return 0;
+}
